561287e02f9e2b3a3857f024e2113590ac7732de
austinxduong <axduong2@gmail.com>
2021-06-17 11:35:51 -0700

    clean up test, first chunk logic waying through

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,11 @@ const app = require('../lib/app');
 
 describe('app routes', () => {
 
-  it('transfer stuff from index.html, and bring in files', async () => {
-      const actual = await request(app).get('/index.html');
-      const expected = '<h1>index.html</h1>';
-  },
+  it('transfer stuff from index.html, and bring in files', async() => {
+    const actual = await request(app).get('/index.html');
+    const expected = '<h1>index.html</h1>';
+
+    expect(actual.text).toEqual(expected);
+  });
 
 });
